87d533ebdb0f97ffae9736d333cc648afb208583
Geoffrey Challen <challen@illinois.edu>
2017-12-02 11:32:12 -0600

    Small cleanup.

--- docs/index.js
+++ docs/index.js
@@ -4,10 +4,10 @@ window.googleLoginHelper
   .config('531745357390-akh3vsslmb6c3jmufjrqqr5qkif95g1i.apps.googleusercontent.com')
   .login(user => {
     $("#output > pre").text(JSON.stringify(user, null, 2))
-    $("#signin").css('visibility', 'hidden')
+    $("#signin").hide()
   })
   .manual(error => {
-    $("#signin").css('visibility', 'visible')
+    $("#signin").show()
   })
 
 $("#start").click(() => {
